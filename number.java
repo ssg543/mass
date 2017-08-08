ec74686dd1f8e2330a24b89e5796683dfbc9ef88
ssg543 <30828810+ssg543@users.noreply.github.com>
2017-08-08 15:55:42 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,16 @@
+# mass
+import java.io,*;
+import java.util.*;
+class number
+{
+public static void main(string args[])
+{
+int a
+if(a>0)
+{
+system.out.println("number is positive,+a)
+}
+else
+{
+system.out.println("number is negative",+a)
+}}}

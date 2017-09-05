6f8e0ef42d0f44de62d8a17fed184377c0543f91
ssg543 <30828810+ssg543@users.noreply.github.com>
2017-09-05 16:01:53 +0530

    Create posi.c

---
+++ posi.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+if(a>0)
+{
+printf("the given number is positive");
+}
+if(a<0)
+{
+printf("the given number is negative");
+}
+else
+{
+printf("zero");
+}}

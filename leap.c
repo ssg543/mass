7f488aff902042d7cf9087e3e4d9abae1391408a
ssg543 <30828810+ssg543@users.noreply.github.com>
2017-09-12 16:06:13 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int year;
+    if(year%4&&year%100&&year&&400)
+    {
+    printf("leaP year");
+}
+else
+{
+    printf("not leap year");
+    
+}
+}
+

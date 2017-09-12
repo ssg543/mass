54129f46cba268b4f8d865f60877bb5c13dca6a9
ssg543 <30828810+ssg543@users.noreply.github.com>
2017-09-12 16:15:32 +0530

    Create sum of nat.c

---
+++ sum of nat.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,sum=0;
+printf("enter the positive numbers");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+sum+=i;
+printf("sum=%d",sum);
+return n
+}
+}

07e99ee5db868b52afd8b12835b689bdc95f9ac4
ssg543 <30828810+ssg543@users.noreply.github.com>
2017-09-12 15:59:38 +0530

    Create great.c

---
+++ great.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the value of a,b,c");
+scanf("%d%d%d",&a&b&c);
+
+if(a>b&&a>c)
+{
+printf("a is greater");
+}
+else if(b>c&&b>a)
+{
+printf("b is greater");
+}
+else
+{
+printf("c is greater");
+}}
+

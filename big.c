0798659260812b18907aed3e409046995ed5a5b0
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-01-09 16:37:00 +0530

    Create big.c

---
+++ big.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int 1,2,3;
+printf("enter the number");
+{
+if(1>2&&1>3)
+{
+printf("1 is big");
+}
+if(2>3)
+{
+printf("2 id big");
+}}
+else
+printf("3is big");
+}

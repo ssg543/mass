a360c59ca9b8fb71511a8aa816af14acdd3d77e1
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-01-11 16:29:22 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a;
+ printf*("enter the value");
+ scanf("%d",&a);
+ if(a%2==0)
+ {
+ printf("even");
+ }
+ else
+ {
+ printf("odd")https://github.com/ssg543/mass/blob/master/pali.c}
+ }
+ }
+ 

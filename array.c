abb15bba2a0f92b8200f9c1f1ab478bbafefd44c
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-01-18 16:00:53 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+add(int,int);
+int main()
+{
+int a1[10],a2[10];
+printf("the given element");
+scanf("%d",&a1);
+printf("the given elements");
+scanf("%d",&a2);
+sum=add();
+}
+add(int a,int b)
+{
+int c;
+printf("the numbers are");
+scanf("%d",a2);
+c=a+b;
+printf("the sum is %d",c);
+	return 0;
+}

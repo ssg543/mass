da1a20391017bc513d3dbb050b50e53f639291e3
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-01-19 08:27:38 +0000

    Create arm.c

---
+++ arm.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int n,n1,remainder=0,sum=0;
+scanf("%d",&n);
+n1=n;
+while(n>0)
+{
+rem=n%10;
+sum=sum+pow(remainder,3);
+n=n/10;
+}
+if(n1==sum)
+printf("yes it is ARMSTRONG");
+else
+printf("no is not ARMSTRONG");
+return 0;
+}

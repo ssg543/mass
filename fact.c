1b60ebdc08c8b7ed366fb8298a81ebe2be175a62
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-01-19 08:39:45 +0000

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+					int n;
+					int i;
+					int fac=1;
+					scanf("%d",&n);
+					for(i=1;i<=n;i++)
+					fac=fac*i;
+					 printf("%d",fac);
+	return 0;
+}

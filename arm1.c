ebf5dfd53f604335e2376d81f5d56ec359a6b0c9
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-01-19 08:37:13 +0000

    Update and rename arm.c to arm1.c

--- arm1.c
+++ arm1.c
@@ -3,13 +3,13 @@
 #include<math.h>
 int main()
 {
-int n,n1,remainder=0,sum=0;
+int n,n1,remader=0,sum=0;
 scanf("%d",&n);
 n1=n;
 while(n>0)
 {
 rem=n%10;
-sum=sum+pow(remainder,3);
+sum=sum+pow(remader,3);
 n=n/10;
 }
 if(n1==sum)

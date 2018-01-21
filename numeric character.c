33b678316dea9836eba695166cf5a325cf76d646
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-01-21 11:37:17 +0530

    Create numeric character.c

---
+++ numeric character.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	char g[100];
+	int h=0,l=1,i;
+	printf("Enter the lines:");
+	gets(a);
+	for(i=0;a[i]!=NULL;i++)
+	{
+		if(a[i]=='.')
+		h=++l;
+	}
+	printf("The number lines is :%d",b);
+	return 0;
+}

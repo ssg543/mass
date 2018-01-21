74151f8848f769733c65ffea1ce890f3c1348e8d
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-01-21 11:25:33 +0530

    Create words in given line.c

---
+++ words in given line.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[100];
+int g=0,i,n;
+printf("Enter the value of the character");
+scanf("%c",&a);
+n=strlen(a);
+for(i=0;i<=n;i++)
+{
+if(a[i]!=" ")
+g++;
+}
+printf("no.of words %d",c+1);
+getch();
+}

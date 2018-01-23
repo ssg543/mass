088306c2460956d65afc2ab2665c32bbda2cc908
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-01-23 16:34:34 +0530

    Create concatenate without strcat.c

---
+++ concatenate without strcat.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    char a,b,c;
+    printf("\nenter the first string");
+    scanf("%c",&a);
+    printf("\nenter the second string");
+    scanf("%c",&b);
+    c= a,b;
+    printf("\nthe concatenate string is%c",c);
+}

56a54adecc85b9caa0ac9293a89e6efdec264a6a
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-01-25 16:37:34 +0530

    Create min,max.c

---
+++ min,max.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int min,max,i,n,a[65];
+    printf("enter the number");
+  
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    a[0]=min=max;
+    if(a[i]>max)
+{
+    printf("\nmax value is:",a[i]);
+}
+   if(a[i]<max)
+   {
+       printf("\nmin value is:",a[i]);
+   }
+   }

77f380e944b7ef268bd7de3e7b2d2c96f9952e82
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-01-25 16:47:55 +0530

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a=2,b=3,c;
+    printf("\nenter a value:%d");
+    scanf("%d",&a);
+    printf("\nenter b value:%d");
+    scanf("%d",&b);
+    c=a+b;
+    printf("\nc value is:%d");
+}

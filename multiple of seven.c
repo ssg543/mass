9d7d10e74fca2832cda375e1d6a54703f19eb5d8
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-02-06 16:49:29 +0530

    Create multiple of seven.c

---
+++ multiple of seven.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int number;
+printf("enter the number:");
+scanf("%d",&number);
+if(number%7==0)
+{
+    printf("multiple of seven");
+}
+else
+{
+    printf("not");
+}}

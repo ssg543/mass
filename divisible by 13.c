03eea96a25bff15f12f2944de8e5609c07063f37
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-02-22 16:34:55 +0530

    Create divisible by 13.c

---
+++ divisible by 13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    if(n%13==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}

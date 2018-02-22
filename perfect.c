a1e4a8cfcdfae984c14fc463dcb4931e6f2e04fd
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-02-22 16:47:41 +0530

    Create perfect.c

---
+++ perfect.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int n,b;
+printf("enter the first value:");
+scanf("%d",&n);
+printf("enter the second value:");
+scanf("%d",&b);
+if(n*n==b)
+{
+    printf("yes");
+}
+else
+{
+    printf("no");
+}
+}

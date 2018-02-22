12d5cdc7e7a2d44ee3c7317539d878a80534e81e
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-02-22 15:58:50 +0530

    Create between.c

---
+++ between.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int l,r,n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    printf("enter l value:");
+    scanf("%d",&l);
+    printf("enter r value:");
+    scanf("%d",&r);
+if(n>l&&n<r)
+{
+    printf("yes");
+}
+else
+{
+    printf("no");
+}
+}

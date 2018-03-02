e1f997ee28293cdcfb430b1f49db6be0ce1426d8
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-03-02 14:52:59 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int n1,n2,i;
+    printf("enter the number1:");
+    scanf("%d",&n1);
+    printf("enter the number2:");
+    scanf("%d",&n2);
+    i=n2-n1;
+    printf("%d",i);
+}
+
+    
+    
+    
+    
+   

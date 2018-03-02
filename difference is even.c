20ac13dc3934ab25936d97f65614820d5b75e51b
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-03-02 14:14:05 +0530

    Create difference is even.c

---
+++ difference is even.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+ int n1,n2;
+ printf("enter the numbers");
+ scanf("%d,%d",&n1,&n2);
+ if((n1-n2)%2==0)
+ {
+     printf("even");
+ }
+ else
+ {
+     printf("not");
+ }
+ }

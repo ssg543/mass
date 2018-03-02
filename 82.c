e99df040de3470859e9e6d4b2a258c849aa9cacb
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-03-02 15:02:01 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n1,n2;
+    float s;
+    printf("enter the first number:");
+    scanf("%d",&n1);
+    printf("enter the second number:");
+    scanf("%d",&n2);
+    s=n1*n2;
+    printf("%.5f",s);
+}
+    
+    
+    

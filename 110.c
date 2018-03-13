0342b5376a1024f6d5853149594728a041f4c9f7
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-03-13 16:51:32 +0530

    Create 110.c

---
+++ 110.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int n,i,sum=0;
+    printf("enter the number:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        sum=sum+i;
+    }
+    printf("natural numbers are %d",sum);
+}

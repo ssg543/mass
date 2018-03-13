1e1228c8fe5955184d558ec6ad3e34e8079c89e2
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-03-13 16:56:43 +0530

    Create ap.c

---
+++ ap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int ap, a,n,a;
+    printf("enter a value:");
+    scanf("%d",&a);
+    printf("enter the n value");
+    scanf("%d",&n);
+    printf("ener the d value");
+    scanf("%d",&d);
+    ap=a+(n-1)*d;
+    printf("ap value is",ap);
+}

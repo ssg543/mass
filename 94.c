9f24faeb162ff20ab378f25717d1a78db06df9d9
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-03-15 14:40:35 +0530

    Create 94.c

---
+++ 94.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    int N[5],K,i;
+    printf("enter the N value:");
+    for(i=0;i<=N[i];i++)
+    {
+        scanf("%d",&N[i]);
+    }
+    printf("enter the K value:");
+    scanf("%d",&K);
+      if(N[i]==K)
+      {
+          printf("number is%d");
+      }
+    }

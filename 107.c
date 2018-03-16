2cc6a025bf5762a792acb53e5c0c31ac4c7dfb31
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-03-16 14:57:41 +0530

    Create 107.c

---
+++ 107.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,d;
+    printf("enter the values:");
+    scanf("%d",&a);
+     printf("enter the values:");
+    scanf("%d",&b);
+    printf("enter the values:");
+    scanf("%d",&c);
+    d=(a*b)/c;
+    printf("%d",d);
+}

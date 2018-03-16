32bfdc17f39ba0ea969d5e692902df7803603078
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-03-16 14:52:23 +0530

    Create 105.c

---
+++ 105.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int n,A=64,i;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        A++;
+    }
+    printf("%d",A);
+    }
+    

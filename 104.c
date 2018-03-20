aa1ce2944edb35d1f8e25e4ea89edffc0027465b
ssg543 <30828810+ssg543@users.noreply.github.com>
2018-03-20 16:50:46 +0530

    Create 104.c

---
+++ 104.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter the a value:");
+    scanf("%d",&a);
+       printf("enter the b value:");
+    scanf("%d",&b);
+   c=a^b;
+   printf("%d",c);
+}
